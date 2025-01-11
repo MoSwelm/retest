e3cea5fd714d9aba1e14871cbcdd03a8cbf3824b
MoSwelm <67298562+MoSwelm@users.noreply.github.com>
2025-01-11 20:58:39 +0200

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="bg-danger">
         <div className="row vh-100 pt-5">
             <div className="col-md-12 text-center text-white">
-                <img width="250px" src="../../../src/assets/avataaars.svg" alt="" />
+                <img width="250px" src="../../src/assets/avataaars.svg" alt="" />
                 <h2 className="text-uppercase fw-bold mt-4">start Framework</h2>
                 <p>Graphic Artist - Web Designer - Illustrator</p>
             </div>
